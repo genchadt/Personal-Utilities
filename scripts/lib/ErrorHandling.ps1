cc385894966915b1ca061897a4f9da82a2046b6c
genchadt <62498018+genchadt@users.noreply.github.com>
2024-02-18 19:11:12 -0500

    testing

--- scripts/lib/ErrorHandling.ps1
+++ scripts/lib/ErrorHandling.ps1
@@ -1,20 +1,6 @@
-function Write-Log {
-    param (
-        [string]$Message,
-        [string]$Type = "Info" # Default type is Info. Other types could be Error, Warning, etc.
-    )
-    switch ($Type) {
-        "Error" { Write-Host -ForegroundColor Red "ERROR: $Message" }
-        "Warning" { Write-Host -ForegroundColor Yellow "WARNING: $Message" }
-        default { Write-Host "INFO: $Message" }
-    }
-}
+. "$PSScriptRoot\TextHandling.ps1"
 
-function Write-Divider {
-    Write-Host "----------------------------------------"
-}
-
-function EnhancedErrorHandling {
+function ErrorHandling {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -30,29 +16,22 @@ function EnhancedErrorHandling {
         [int]$OffsetInLine = $MyInvocation.OffsetInLine
     )
 
-    Write-Divider
-    Write-Log -Message "Error: $ErrorMessage" -Type Error
-    Write-Log -Message "StackTrace: $StackTrace" -Type Error
+    Write-Divider -Strong
+    Write-Console -Text "Error: $ErrorMessage"
+    Write-Console -Text "StackTrace: $StackTrace"
     if ($ScriptName) {
-        Write-Log -Message "Exception Source: $ScriptName" -Type Error
+        Write-Console -Text "Exception Source: $ScriptName"
     }
     if ($ScriptLineNumber -ne 0) {
-        Write-Log -Message "Exception Line: $ScriptLineNumber" -Type Error
+        Write-Console -Text "Exception Line: $ScriptLineNumber"
     }
     if ($OffsetInLine -ne 0) {
-        Write-Log -Message "Exception Offset: $OffsetInLine" -Type Error
+        Write-Console -Text "Exception Offset: $OffsetInLine"
     }
-    Write-Divider
+    Write-Divider -Strong
 
-    # Consider using Write-Error for non-terminating errors or throw for terminating errors with specific messages
-    $exception = New-Object System.Exception "Error executing command: $ErrorMessage"
-    $PSCmdlet.ThrowTerminatingError($exception)
-}
-
-# Example usage within a try-catch block
-try {
-    # Simulate an operation that could fail
-    throw "Simulated failure"
-} catch {
-    EnhancedErrorHandling -ErrorMessage $_.Exception.Message -StackTrace $_.Exception.StackTrace
+    # Optionally, re-throw or handle the error based on script requirements
+    # throw "Error executing command: $ErrorMessage"
+    # Or log the error without stopping execution
+    Write-Log -Message "An error occurred: $ErrorMessage"
 }
