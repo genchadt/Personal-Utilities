39ffc79b85832016ba025112a2873ad7852365df
genchadt <62498018+genchadt@users.noreply.github.com>
2024-03-19 11:58:50 -0400

    New script: Install-Fonts

---
+++ scripts/Install-Fonts.ps1
@@ -0,0 +1,47 @@
+###############################################
+# Install-Fonts
+#
+# Installs fonts recursively from the current directory.
+###############################################
+
+###############################################
+# Imports
+###############################################
+
+. "$PSScriptRoot\lib\ErrorHandling.ps1"
+. "$PSScriptRoot\lib\TextHandling.ps1"
+. "$PSScriptRoot\lib\SysOperation.ps1"
+
+Write-Console "Starting font installations..."
+$fontsFolder = (New-Object -ComObject Shell.Application).Namespace(0x14)
+$fontFiles = Get-ChildItem -Recurse -Filter *.ttf
+
+$jobs = @()
+
+foreach ($file in $fontFiles) {
+    $job = Start-Job -ScriptBlock {
+        param($file, $fontsFolder)
+        $fileName = $file.Name
+        $fontPath = $file.FullName
+        $destinationPath = "C:\Windows\fonts\$fileName"
+
+        if (-not(Test-Path -Path $destinationPath)) {
+            Write-Console "Installing font: $fileName"
+            $fontsFolder.CopyHere($fontPath)
+        } else {
+            Write-Console "Font already installed: $fileName"
+        }
+    } -ArgumentList $file, $fontsFolder
+    $jobs += $job
+}
+
+# Wait for all jobs to complete
+$jobs | Wait-Job
+
+# Output any results from the jobs
+$jobs | ForEach-Object {
+    Receive-Job -Job $_
+}
+
+# Clean up the jobs
+$jobs | Remove-Job
