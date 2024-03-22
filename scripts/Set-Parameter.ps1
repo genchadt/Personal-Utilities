82e6b5764e8ca69700a85137333e0a3db7146955
genchadt <62498018+genchadt@users.noreply.github.com>
2024-03-22 11:32:37 -0400

    Doc string

--- scripts/Set-Parameter.ps1
+++ scripts/Set-Parameter.ps1
@@ -1,3 +1,58 @@
+<#
+.SYNOPSIS
+    Set-Parameter.PS1 - Modifies file and folder attributes in the specified directory or the current working directory.
+
+.DESCRIPTION
+    This script allows for the modification of file and folder attributes within the specified paths or the current working directory by default. It can add or remove attributes such as Hidden, ReadOnly, etc., to files and folders, with optional recursion into subdirectories.
+
+.PARAMETER Path
+    Specifies the path(s) where the file and folder attributes will be modified. Defaults to the current working directory (PWD) if not specified.
+
+.PARAMETER Add
+    Specifies the attributes to add to the files or folders. Accepts multiple values separated by commas.
+
+.PARAMETER Remove
+    Specifies the attributes to remove from the files or folders. Accepts multiple values separated by commas.
+
+.PARAMETER Recurse
+    If specified, the script will recursively modify attributes in all subdirectories of the path.
+
+.PARAMETER Force
+    If specified, the script will overwrite existing attributes without prompting for confirmation.
+
+.PARAMETER NoLog
+    Suppresses logging of attribute changes. By default, changes are logged.
+
+.EXAMPLE
+    .\Set-Parameter.ps1 -Add Hidden,ReadOnly
+    Adds the Hidden and ReadOnly attributes to files and folders in the current working directory.
+
+.EXAMPLE
+    .\Set-Parameter.ps1 -Remove Hidden -Path C:\MyFolder -Recurse
+    Removes the Hidden attribute from all files and folders within 'C:\MyFolder' and its subdirectories.
+
+.EXAMPLE
+    .\Set-Parameter.ps1 -Add System -Path .\config.sys -NoLog
+    Adds the System attribute to 'config.sys' in the current directory without logging the change.
+
+.INPUTS
+    None. You cannot pipe objects to Set-Parameter.ps1.
+
+.OUTPUTS
+    String. Outputs logs of attribute changes unless NoLog is specified.
+
+.LINK
+    About_Attributes: [Microsoft Documentation on FileAttributes](https://docs.microsoft.com/en-us/dotnet/api/system.io.fileattributes)
+
+.NOTES
+    Use this script with caution, especially when modifying system or hidden attributes, as it can affect file and folder visibility and behavior.
+
+    Script Version: 1.0.0
+    Author: Chad
+    Creation Date: 2024-03-22 03:30:00 GMT
+    Last Updated: 2024-03-22 03:30:00 GMT
+#>
+
 ###############################################
 # Parameters
 ###############################################
