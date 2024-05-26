b088b65a43470ef599c95b0a58d91f1ab960d2ee
genchadt <62498018+genchadt@users.noreply.github.com>
2024-05-25 20:59:03 -0400

    Backup feature

--- scripts/Add-ToSystemPath.ps1
+++ scripts/Add-ToSystemPath.ps1
@@ -14,9 +14,38 @@
 
 #>
 
+###############################################
+# Parameters
+###############################################
+
 param (
     [string[]]$PathsToAdd
 )
+
+###############################################
+# Imports
+###############################################
+
+Import-Module "$PSScriptRoot\lib\ErrorHandling.psm1"
+Import-Module "$PSScriptRoot\lib\SysOperation.psm1"
+Import-Module "$PSScriptRoot\lib\TextHandling.psm1"
+
+###############################################
+# Functions
+###############################################
+
+function Backup-SystemPath {
+    param (
+        [string]$BackupPath = "$PSScriptRoot\backups\path.reg"
+    )
+
+    if ($BackupPath -notlike "*.reg") {
+        $BackupPath += ".reg"
+    }
+
+    $current_path = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+    $current_path | Out-File "$PSScriptRoot\backups\path.reg"
+}
 
 function Join-SystemPath {
     param (
@@ -36,13 +65,15 @@ function Add-ToSystemPath {
     foreach ($Path in $PathsToAdd) {
         if ($current_path -notlike "*$Path*") {
             $current_path += ";$Path"
-            Write-Output "Added path: $Path"
+            Write-Console "Added path: $Path"
         } else {
-            Write-Output "Path already present: $Path"
+            Write-Console "Path already present: $Path" -MessageType Warning
         }
     }
 
+    Backup-SystemPath
     Join-SystemPath -Paths $current_path
+    Write-Console "New path: $current_path"
 }
 
 $params = @{
