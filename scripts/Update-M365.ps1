bbcea546514deacfd15cd8398903b1246e408588
genchadt <62498018+genchadt@users.noreply.github.com>
2024-10-20 09:12:37 -0400

    Try catch

--- scripts/Update-M365.ps1
+++ scripts/Update-M365.ps1
@@ -1,1 +1,6 @@
-& "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
+try {
+    & "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update user
+}
+catch {
+    Write-Warning "Failed to start Office update process: $_"
+}
