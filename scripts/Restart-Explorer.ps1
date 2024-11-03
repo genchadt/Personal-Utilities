177643b80d71f2b056d2071771cce123ac432945
genchadt <62498018+genchadt@users.noreply.github.com>
2024-11-02 22:37:43 -0400

    initial commit

---
+++ scripts/Restart-Explorer.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+Restart the Windows Explorer process.
+
+.DESCRIPTION
+Restart the Windows Explorer process, which is necessary after certain system
+settings have been changed.
+
+.EXAMPLE
+Restart-Explorer
+#>
+function Restart-Explorer {
+    # Restart the Windows Explorer process.
+    Write-Host "Restarting Explorer.exe..."
+    Stop-Process -Name explorer -Force
+    # Give the process some time to shut down.
+    Start-Sleep -Milliseconds 500
+    # Start the Explorer process again.
+    Start-Process explorer.exe
+}
+
+# Restart the Explorer process.
+Restart-Explorer
